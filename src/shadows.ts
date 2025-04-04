5eeb9796fd50f45ef8b21b17f037384e2779b7ac
eslam bayome <eslamalaa532@gmail.com>
2025-04-04 15:17:36 +0200

    add haunted house scene with lighting and animated sphere

--- src/shadows.ts
+++ src/shadows.ts
@@ -2,6 +2,13 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
 
+const textureLoader = new THREE.TextureLoader();
+const backedShadow = textureLoader.load(
+  "/public/static/textures/bakedShadow.jpg"
+);
+const simpleShadow = textureLoader.load(
+  "/public/static/textures/simpleShadow.jpg"
+);
 /**
  * Base
  */
@@ -18,12 +25,12 @@ const scene = new THREE.Scene();
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(2, 2, -1);
 gui.add(directionalLight, "intensity").min(0).max(3).step(0.001);
 gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
@@ -38,13 +45,42 @@ directionalLight.shadow.camera.left = -2;
 directionalLight.shadow.camera.right = 2;
 directionalLight.shadow.camera.top = 2;
 directionalLight.shadow.camera.bottom = -2;
-directionalLight.shadow.radius = 10;
+// directionalLight.shadow.radius = 10;
+
 const lightCameraHelper = new THREE.CameraHelper(
   directionalLight.shadow.camera
 );
 // scene.add(lightCameraHelper);
 scene.add(directionalLight);
 
+//spotlight
+const spotLight = new THREE.SpotLight(0xffffff, 1.4, 10, Math.PI * 0.3);
+spotLight.castShadow = true;
+spotLight.position.set(0, 1, 1);
+scene.add(spotLight);
+scene.add(spotLight.target);
+spotLight.shadow.camera.far = 7;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.fov = 30;
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+spotLightCameraHelper.visible = false;
+scene.add(spotLightCameraHelper);
+
+// point light
+const pointLight = new THREE.PointLight("white", 1.3);
+pointLight.castShadow = true;
+pointLight.position.set(-1, 1, 0);
+pointLight.shadow.camera.far = 5;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+scene.add(pointLight);
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
 /**
  * Materials
  */
@@ -66,6 +102,17 @@ plane.position.y = -0.5;
 plane.receiveShadow = true;
 scene.add(sphere, plane);
 
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneGeometry(1.5, 1.5),
+  new THREE.MeshBasicMaterial({
+    color: "black",
+    transparent: true,
+    alphaMap: simpleShadow,
+  })
+);
+sphereShadow.rotation.x = -Math.PI * 0.5;
+sphereShadow.position.y = plane.position.y + 0.01;
+scene.add(sphereShadow);
 /**
  * Sizes
  */
@@ -113,7 +160,8 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = false;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -124,6 +172,16 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.position.z = Math.sin(elapsedTime * 0.8) * 1.5;
+  sphere.position.x = Math.cos(elapsedTime * 0.8) * 1.5;
+  sphere.position.y = Math.abs(Math.sin(elapsedTime * 1.8));
+
+  //update the shadow
+  sphereShadow.position.x = sphere.position.x;
+  sphereShadow.position.z = sphere.position.z;
+  sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3;
 
   // Update controls
   controls.update();
